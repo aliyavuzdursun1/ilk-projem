b1a214bdaef03cead74e6dd7177da099494036a5
Ali Yavuz Dursun <aliyavuzdursun4@gmail.com>
2026-03-10 16:55:34 +0300

    Dosya islemleri ve gunluk ozelligi eklendi

--- merhaba.py
+++ merhaba.py
@@ -1,22 +1,21 @@
-import requests
+def gunluk_yaz():
+    not_icerigi = input("Bugün ne öğrendin? : ")
+    
+    # 'a' (append) modu: Dosyanın sonuna ekleme yapar, eskiyi silmez.
+    with open("gunluk.txt", "a", encoding="utf-8") as dosya:
+        dosya.write("- " + not_icerigi + "\n")
+    print("✅ Notun 'gunluk.txt' dosyasına kaydedildi!")
 
-def kur_getir():
-    print("🌐 Merkez Bankası verilerine bağlanılıyor...")
-    
-    # Döviz kurlarını veren ücretsiz bir adrese gidiyoruz
-    url = "https://api.exchangerate-api.com/v4/latest/USD"
-    
+def gunluk_oku():
+    print("\n--- SENİN MÜHENDİSLİK GÜNLÜĞÜN ---")
     try:
-        cevap = requests.get(url)
-        veriler = cevap.json() # Gelen karmaşık yazıyı Python'ın anlayacağı sözlüğe çeviriyoruz
-        
-        dolar_tl = veriler["rates"]["TRY"]
-        
-        print("---------------------------")
-        print("💵 1 ABD Doları = " + str(dolar_tl) + " TL")
-        print("---------------------------")
-        
-    except:
-        print("❌ İnternet bağlantısı kurulamadı veya bir hata oluştu.")
+        with open("gunluk.txt", "r", encoding="utf-8") as dosya:
+            icerik = dosya.read()
+            print(icerik)
+    except FileNotFoundError:
+        print("⚠️ Henüz bir günlük dosyası oluşturulmamış.")
+    print("---------------------------------")
 
-kur_getir()
+# Programı çalıştıralım
+gunluk_yaz()
+gunluk_oku()
